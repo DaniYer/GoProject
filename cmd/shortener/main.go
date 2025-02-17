f3b522ed13afb8327c2bde1e4db8439656748773
DaniYer <karioko767.se87@gmail.com>
2025-02-17 14:58:10 +0500

    iter6

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -5,25 +5,35 @@ import (
 	"io"
 	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/DaniYer/GoProject.git/internal/app/config"
 	"github.com/go-chi/chi/v5"
+	"go.uber.org/zap"
 )
 
 var storage = map[string]string{}
+var sugar *zap.SugaredLogger
 
 func main() {
 
+	// создаём предустановленный регистратор zap
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		// вызываем панику, если ошибка
+		panic(err)
+	}
+	defer logger.Sync()
 	// Получаем конфигурацию из пакета config
 	cfg := config.NewConfig()
-
+	sugar = logger.Sugar()
 	// Выводим конфигурацию (для отладки)
 	cfg.Print()
 	r := chi.NewRouter()
-	r.Post("/", func(w http.ResponseWriter, r *http.Request) {
+	r.Post("/", WithLogging(func(w http.ResponseWriter, r *http.Request) {
 		shortenedURL(w, r, cfg.BaseURL)
-	})
-	r.Get("/{id}", redirectedURL)
+	}))
+	r.Get("/{id}", WithLogging(redirectedURL))
 	fmt.Println(cfg.ServerAddress)
 	if err := http.ListenAndServe(cfg.ServerAddress, r); err != nil {
 		panic(err)
@@ -83,4 +93,59 @@ func genSym() string {
 		result += string(rune(rand.Intn(26) + 'a')) // Генерация случайных символов
 	}
 	return result
+}
+
+type (
+	// берём структуру для хранения сведений об ответе
+	responseData struct {
+		status int
+		size   int
+	}
+
+	// добавляем реализацию http.ResponseWriter
+	loggingResponseWriter struct {
+		http.ResponseWriter // встраиваем оригинальный http.ResponseWriter
+		responseData        *responseData
+	}
+)
+
+func (r *loggingResponseWriter) Write(b []byte) (int, error) {
+	// записываем ответ, используя оригинальный http.ResponseWriter
+	size, err := r.ResponseWriter.Write(b)
+	r.responseData.size += size // захватываем размер
+	return size, err
+}
+
+func (r *loggingResponseWriter) WriteHeader(statusCode int) {
+	// записываем код статуса, используя оригинальный http.ResponseWriter
+	r.ResponseWriter.WriteHeader(statusCode)
+	r.responseData.status = statusCode // захватываем код статуса
+}
+
+func WithLogging(h http.HandlerFunc) http.HandlerFunc {
+
+	logFn := func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		responseData := &responseData{
+			status: 0,
+			size:   0,
+		}
+		lw := loggingResponseWriter{
+			ResponseWriter: w, // встраиваем оригинальный http.ResponseWriter
+			responseData:   responseData,
+		}
+		h(&lw, r) // внедряем реализацию http.ResponseWriter
+
+		duration := time.Since(start)
+
+		sugar.Infoln(
+			"uri", r.RequestURI,
+			"method", r.Method,
+			"status", responseData.status, // получаем перехваченный код статуса ответа
+			"duration", duration,
+			"size", responseData.size, // получаем перехваченный размер ответа
+		)
+	}
+	return logFn
 }
